0dafda19d7f8c2d61711c9cffe3a8d577e62c29e
k21062319 <muhammad.r.rahman@kcl.ac.uk>
2022-10-16 21:30:50 +0100

    Fix missing bracket in naem attribute of Thing model

--- things/models.py
+++ things/models.py
@@ -6,7 +6,7 @@ class Thing(models.Model):
         max_length=30,
         unique=True,
         blank=False,
-        )])
+    )
     description = models.CharField(
         max_length=120,
         blank=True,
