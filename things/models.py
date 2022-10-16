a84d87eb7bf2faff12081a5620ac54e97550a5ed
k21062319 <muhammad.r.rahman@kcl.ac.uk>
2022-10-16 20:57:20 +0100

    Add restrictions to name attribute

--- things/models.py
+++ things/models.py
@@ -1,6 +1,13 @@
+from django.core.validators import RegexValidator
 from django.db import models
 
 class Thing(models.Model):
-    name = models.TextField()
+    name = models.TextField(
+        max_length=30,
+        unique=True,
+        validators=[RegexValidator(
+            regex=r'^\w{1,}$',
+            message='Name cannot be be empty and must be 30 characters or less'
+        )])
     description = models.TextField()
     quantity = models.IntegerField(default=0)
