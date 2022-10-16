a9cf85d4c7c44d72a478dac76af3fa283e92ff93
k21062319 <muhammad.r.rahman@kcl.ac.uk>
2022-10-16 01:53:55 +0100

    Create Thing model again

--- things/models.py
+++ things/models.py
@@ -1,7 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
-class Thing(AbstractUser):
+class Thing(models.Model):
     name = models.TextField()
     description = models.TextField()
-    quantity = models.TextField()
+    age = models.TextField()
